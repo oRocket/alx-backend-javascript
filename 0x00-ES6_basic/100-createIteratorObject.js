d2e2acbf82936783166fc452ce40c3cc30b45e24
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-04-24 17:32:31 +0000

    correct indentation

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,7 @@
 export default function createIteratorObject(report) {
   const myarray = [];
-    for (const item of Object.values(report.allEmployees)) {
-      myarray.push(...item);
-    }
+  for (const item of Object.values(report.allEmployees)) {
+    myarray.push(...item);
+  }
   return myarray;
 }
