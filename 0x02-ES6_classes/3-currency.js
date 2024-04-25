231d41d50e59c5966b6686ad409d07380c137e88
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-04-25 19:13:29 +0000

    indent

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,21 +1,21 @@
 export default class Currency {
   constructor(code, name) {
-  this._code = code;
-  this._name = name;
+    this._code = code;
+    this._name = name;
   }
   get code() {
-  return this._code;
+    return this._code;
   }
   set code(newCode) {
-  this._code = newCode;
+    this._code = newCode;
   }
   get name() {
-  return this._name;
+    return this._name;
   }
   set name(newName) {
-  this._name = newName;
+    this._name = newName;
   }
   displayFullCurrency() {
-  return `${this._name} (${this._code})`;
+    return `${this._name} (${this._code})`;
   }
 }
