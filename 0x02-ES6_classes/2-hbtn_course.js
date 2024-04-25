f965683226ea0502c49c4d45adcc77212ffd9da7
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-04-25 22:21:27 +0000

    indentation

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -10,20 +10,24 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
+
   get name() {
     return this._name;
   }
+
   set name(newName) {
     if (typeof newName !== 'string') {
     throw new TypeError('Name must be a string');
     }
     this._name = newName;
   }
+
   get length() {
     return this._length;
   }
+
   get students() {
     return this._students;
   }
 }
-  
+ 
