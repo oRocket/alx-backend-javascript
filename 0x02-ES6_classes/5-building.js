c253df8ef00fd6b465d35a34a6bbd3a3b9d57dcd
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-04-25 19:32:40 +0000

    A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,11 +1,21 @@
 export default class Building {
-  constructor(sqft) {
-    this._sqft = sqft;
+    constructor(sqft) {
+      this.sqft = sqft;
+      if (this.constructor !== Building) {
+        if (typeof this.evacuationWarningMessage !== 'function') {
+          throw new Error(
+            'Class extending Building must override evacuationWarningMessage',
+          );
+        }
+      }
+    }
+  
+    get sqft() {
+      return this._sqft;
+    }
+  
+    set sqft(value) {
+      this._sqft = value;
+    }
   }
-  get sqft() {
-    return this._sqft;
-  }
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
-}
+  
