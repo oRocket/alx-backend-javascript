e58c577658df6a3d0e6ace045e1e619b908b8626
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-04-25 20:10:58 +0000

    indent

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -3,18 +3,23 @@ export default class HolbertonClass {
     this.size = size;
     this.location = location;
   }
+
   get size() {
     return this._size;
   }
+
   set size(value) {
     this._size = value;
   }
+
   get location() {
     return this._location;
   }
+
   set location(value) {
     this._location = value;
   }
+  
   [Symbol.toPrimitive](hint) {
     if (hint === 'number') {
       return this.size;
