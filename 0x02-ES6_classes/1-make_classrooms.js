1dd2cf8d635f1007c1f4fa90e0fcda83cc54612a
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-04-25 22:18:44 +0000

    fixed

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,4 +1,4 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 function initializeRooms() {
   const room1 = new ClassRoom(19);
@@ -6,4 +6,5 @@ function initializeRooms() {
   const room3 = new ClassRoom(34);
   return [room1, room2, room3];
 }
+
 export default initializeRooms;
