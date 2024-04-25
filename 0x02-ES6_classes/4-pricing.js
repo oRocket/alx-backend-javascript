29eb2ac50ab3785067b94c46938df13af228c432
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-04-25 19:24:10 +0000

    Pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -5,28 +5,22 @@ export default class Pricing {
     this._amount = amount;
     this._currency = currency;
   }
-
   get amount() {
     return this._amount;
   }
-
   set amount(newAmount) {
     this._amount = newAmount;
   }
-
   get currency() {
     return this._currency;
   }
-
   set currency(newCurrency) {
     this._currency = newCurrency;
   }
-
   displayFullPrice() {
     const { _name, _code } = this._currency;
     return `${this._amount} ${_name} (${_code})`;
   }
-
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
