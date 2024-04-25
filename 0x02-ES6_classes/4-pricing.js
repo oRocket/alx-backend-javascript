e3740e81f46ad1bdf2cd24c99ce4d7fc35262ef9
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-04-25 22:39:59 +0000

    A Building

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -30,5 +30,4 @@ export default class Pricing {
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
-
 }
--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -9,11 +9,11 @@ export default class Building {
       }
     }
   }
-  
+
   get sqft() {
     return this._sqft;
   }
-  
+
   set sqft(value) {
     this._sqft = value;
   }
