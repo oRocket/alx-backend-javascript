648ac48c18dbc79bf6c01fb827e04342d9cc7b4e
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-04-25 15:57:47 +0000

    Methods, static methods, computed methods names..... MONEY

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,25 +3,19 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
-  
   get code() {
     return this._code;
   }
-  
   set code(newCode) {
     this._code = newCode;
   }
-  
   get name() {
     return this._name;
   }
-  
   set name(newName) {
     this._name = newName;
   }
-  
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
 }
-  
