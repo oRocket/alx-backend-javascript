ef8cf0c388a0013711130199edb9d14dc18b49db
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-04-30 11:53:48 +0000

    Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -26,6 +26,6 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
           .filter((grade) => grade.studentId === student.id)
           .pop() || defaultGrade).grade,
       }));
-    }
+  }
   return [];
 }
